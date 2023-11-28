205ec6f329ca20a223a217a3b146be23e75fa6e8
malladadani <138223991+malladadani@users.noreply.github.com>
2023-11-28 18:22:13 +0100

    Update Programa_Interrupcion.ino

--- Programa_Interrupcion.ino
+++ Programa_Interrupcion.ino
@@ -1,21 +1,32 @@
+/*
+  Programa para cambiar el estado de dos led con una interrupción externa
+  */
+
+// Definición de constantes
 #define SALIDA 15
 #define Boton 33
 
 #define LED_1 24
 #define LED_2 27
 
+// Variable para el cambio de estado
 int cont = 0;
 
+// Configuración
 void setup() {
   // put your setup code here, to run once:
+
+  // Configuración de pines
   pinMode(Boton, INPUT);
   pinMode(SALDIA, OUTPUT);
 
   pinMode(LED_1,OUTPUT);
   pinMode(LED_2,OUTPUT);
 
+  // Configuración de baudios salida
   Serial.begin(9600);
 
+  // Configuración de la interrupción, por flanco de bajada
   attachInterrupt(digitalPinToInterrupt(Boton), funcion_interrupcion, LOW);
 
 }
@@ -48,6 +59,7 @@ void loop() {
 
 void funcion_interrupcion()
 {
+  // Delay para el rebote
   delay(20);
   if(cont == 3)
   {
